e49223a469dfe778a9c4193a2bde480ced77b6ad
Tilin <jm293229@gmail.com>
2025-06-18 16:53:15 -0600

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -6,7 +6,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`*[❗] Ey @${m.sender.split`@`[0]}, no vez que soy un bot si quieres comprar un bot contacté con mi creador https://wa.me/+528336105471
+    await m.reply(`*[❗] 𝑯𝑶𝑳𝑨 @${m.sender.split`@`[0]}, 𝑺𝑶𝒀 𝑩𝑶𝑻 𝑵𝑶 𝑴𝑨𝑵𝑫𝑬𝑺 𝑷𝑹𝑰𝑽𝑨𝑫𝑶 𝑺𝑰 𝑸𝑼𝑰𝑬𝑹𝑺 𝑪𝑶𝑴𝑷𝑹𝑨 𝑼𝑵 𝑩𝑶𝑻 𝑴𝑨𝑵𝑫𝑨 𝑴𝑬𝑵𝑺𝑨𝑱𝑬 𝑨 𝑴𝑰 𝑫𝑼𝑬𝑵̃𝑶 https://wa.me/+525538375800
     *`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
